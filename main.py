b005c59229eed12b0b096749e7da1c0bc1c11bcc
shyamal11 <mishra.shyamal11@gmail.com>
2025-05-18 19:22:57 -0400

    initial commit

--- main.py
+++ main.py
@@ -12,3 +12,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
